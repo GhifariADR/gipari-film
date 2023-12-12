3e78a0d67b4ccad3ca34de0d75043acc8a7e42cd
Muhammad Ghifari Adrian <ghifariadr6@gmail.com>
2023-12-12 14:29:17 +0700

    initial commit

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const getMovieList = async () => {
     const movie = await axios.get(`${process.env.REACT_APP_BASEURL}/movie/popular?page=1&api_key=${process.env.REACT_APP_APIKEY}`)
+    console.log(movie.data.results)
     return movie.data.results
 }
 
